c688da9b9a24b81751025ded8755f661b76e0ce2
Jan Arnel Banaag <jhonarnel476@gmail.com>
2025-05-15 07:28:38 +0800

    added incrementTrackViews REST function to TrackAPI class

--- server/src/datasources/track-api.ts
+++ server/src/datasources/track-api.ts
@@ -1,27 +1,31 @@
 import { RESTDataSource } from "@apollo/datasource-rest";
-import { TrackModel, AuthorModel, ModuleModel } from "../models"
+import { TrackModel, AuthorModel, ModuleModel } from "../models";
 
 export class TrackAPI extends RESTDataSource {
-  // the Catstronauts catalog is hosted on this server
-  baseURL = "https://odyssey-lift-off-rest-api.herokuapp.com/";
-  
-  getTracksForHome() {
-    return this.get<TrackModel[]>("tracks");
-  }
+	// the Catstronauts catalog is hosted on this server
+	baseURL = "https://odyssey-lift-off-rest-api.herokuapp.com/";
 
-  getAuthor(authorId: string) {
-    return this.get<AuthorModel>(`author/${authorId}`);
-  }
+	getTracksForHome() {
+		return this.get<TrackModel[]>("tracks");
+	}
 
-  getTrack(trackId: string) {
-    return this.get<TrackModel>(`track/${trackId}`);
-  }
+	getAuthor(authorId: string) {
+		return this.get<AuthorModel>(`author/${authorId}`);
+	}
 
-  getTrackModules(trackId: string) {
-    return this.get<ModuleModel[]>(`track/${trackId}/modules`);
-  }
-  
-  getModule(moduleId: string) {
-    return this.get<ModuleModel>(`module/${moduleId}`);
-  }
+	getTrack(trackId: string) {
+		return this.get<TrackModel>(`track/${trackId}`);
+	}
+
+	getTrackModules(trackId: string) {
+		return this.get<ModuleModel[]>(`track/${trackId}/modules`);
+	}
+
+	getModule(moduleId: string) {
+		return this.get<ModuleModel>(`module/${moduleId}`);
+	}
+
+	incrementTrackViews(trackId: String) {
+		return this.patch<TrackModel>(`track/${trackId}/numberOfViews`);
+	}
 }
