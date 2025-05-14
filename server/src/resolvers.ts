a4681fe1994c0d1edba6fa662a83b4bd922c764a
Jan Arnel Banaag <jhonarnel476@gmail.com>
2025-05-15 07:56:12 +0800

    added mutation resolver

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -1,29 +1,43 @@
 import { Resolvers } from "./types";
 
 export const resolvers: Resolvers = {
-  Query: {
-    // returns an array of Tracks that will be used to populate the homepage grid of our web client
-    tracksForHome: (_, __, { dataSources }) => {
-      return dataSources.trackAPI.getTracksForHome();
-    },
+	Query: {
+		// returns an array of Tracks that will be used to populate the homepage grid of our web client
+		tracksForHome: (_, __, { dataSources }) => {
+			return dataSources.trackAPI.getTracksForHome();
+		},
 
-    // get a single track by ID, for the track page
-    track: (_, { id }, { dataSources }) => {
-      return dataSources.trackAPI.getTrack(id);
-    },
+		// get a single track by ID, for the track page
+		track: (_, { id }, { dataSources }) => {
+			return dataSources.trackAPI.getTrack(id);
+		},
 
-    // get a single module by ID, for the module detail page
-    module: (_, { id }, { dataSources }) => {
-      return dataSources.trackAPI.getModule(id);
-    },
-  },
-  Track: {
-    author: ({ authorId }, _, { dataSources }) => {
-      return dataSources.trackAPI.getAuthor(authorId);
-    },
+		// get a single module by ID, for the module detail page
+		module: (_, { id }, { dataSources }) => {
+			return dataSources.trackAPI.getModule(id);
+		},
+	},
 
-    modules: ({ id }, _, { dataSources }) => {
-      return dataSources.trackAPI.getTrackModules(id);
-    },
-  },
+	Mutation: {
+		incrementTrackView: async (_, { id }, { dataSources }) => {
+			const track = await dataSources.trackAPI.incrementTrackViews(id);
+
+			return {
+				code: 200,
+				success: true,
+				message: `Successfully incremented number of views for track ${id}`,
+				track,
+			};
+		},
+	},
+
+	Track: {
+		author: ({ authorId }, _, { dataSources }) => {
+			return dataSources.trackAPI.getAuthor(authorId);
+		},
+
+		modules: ({ id }, _, { dataSources }) => {
+			return dataSources.trackAPI.getTrackModules(id);
+		},
+	},
 };
